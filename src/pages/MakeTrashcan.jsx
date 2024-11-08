9fd6bbc8a069ccc61d8ce037115834c4cbc6aceb
2zooo0 <c20221460@cau.ac.kr>
2024-11-09 03:14:05 +0900

    Feat : 카카오로그인 백 연결필요

---
+++ src/pages/MakeTrashcan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MakeTrashcan = () => {
+  return (
+    <div>
+      <img src="/trash.png" style={{ maxWidth: '100%', height: 'auto' }} />
+    </div>
+  );
+};
+
+export default MakeTrashcan;
