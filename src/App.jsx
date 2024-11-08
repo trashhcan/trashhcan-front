2b2c4af7ab3220f4369bf7341b21f57b695aa89b
dev-joeysos <joeysos@naver.com>
2024-11-08 20:47:53 +0900

    feat: Mobile VH setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Outside>
+      <AppDom>
+        Testing
+      </AppDom>
+    </Outside>
   )
 }
 
 export default App
+
+const AppDom = styled.div`
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-color: #eaeaea;
+
+  @media (max-width: 600px) {
+    width: 100vw;
+    height: 100dvh;
+  }
+`;
+
+const Outside = styled.div`
+  background-color: white;
+`;
