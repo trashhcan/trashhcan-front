7c73835a69c7a2d3b68b00a64abf69f0adefc2ae
dev-joeysos <joeysos@naver.com>
2024-11-19 23:59:16 +0900

    fix: Address

--- src/api/subApi.js
+++ src/api/subApi.js
@@ -1,8 +1,8 @@
 //쓸모없는 랜덤 주제 가져오기 (버튼 클릭 최초 한번만 수행
 import axios from 'axios';
 
-const BASE_URL = 'http://trashhcan-dev.p-e.kr:8080'; // 개발용
-// const BASE_URL = 'https://trashhcandoit.p-e.kr'; //배포용
+// const BASE_URL = 'http://trashhcan-dev.p-e.kr:8080'; // 개발용
+const BASE_URL = 'https://trashhcandoit.p-e.kr'; //배포용
 
 export const fetchRandomSubTitles = async () => {
   try {
